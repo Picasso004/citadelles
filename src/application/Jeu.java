8a9d441e3fcb415bffe5e792df8297edb41a37f7
aminata <adiop.ir2025@esaip.org>
2023-12-06 15:30:53 +0100

    la METHODE CHOIX DE PERSONNAGE

--- src/application/Jeu.java
+++ src/application/Jeu.java
@@ -2,9 +2,7 @@ package application;
 
 import modele.*;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Jeu {
     private PlateauDeJeu plateauDeJeu;
@@ -111,7 +109,41 @@ public class Jeu {
     }
     private void choixPersonnages(){
         //TODO implémenter la méthode
-    }
+            System.out.println("Choix des personnages :");
+
+            for (Joueur joueur : plateauDeJeu.getListeJoueurs()) {
+                ArrayList<Personnage> personnagesRestants = new ArrayList<>(List.of(plateauDeJeu.getListePersonnages()));
+
+                // Écarte deux cartes face cachée et une face visible
+                Collections.shuffle(personnagesRestants);
+                Personnage carteVisible = personnagesRestants.remove(0);
+                Personnage carteCachee1 = personnagesRestants.remove(0);
+                Personnage carteCachee2 = personnagesRestants.remove(0);
+
+                System.out.println("Le personnage \"" + carteVisible.getNom() + "\" est écarté face visible");
+                System.out.println("Le personnage \"" + carteCachee1.getNom() + "\" est écarté face cachée");
+                System.out.println("Le personnage \"" + carteCachee2.getNom() + "\" est écarté face cachée");
+
+                joueur.setPersonnage(choisirPersonnage(personnagesRestants));
+            }
+        }
+
+        private Personnage choisirPersonnage(ArrayList<Personnage> personnagesRestants) {
+            System.out.println("Personnages restants :");
+            for (int i = 0; i < personnagesRestants.size(); i++) {
+                System.out.println((i + 1) + ". " + personnagesRestants.get(i).getNom());
+            }
+
+            Scanner scanner = new Scanner(System.in);
+            int choix;
+            do {
+                System.out.print("Choisissez un personnage (1-" + personnagesRestants.size() + ") : ");
+                choix = scanner.nextInt();
+            } while (choix < 1 || choix > personnagesRestants.size());
+
+            return personnagesRestants.get(choix - 1);
+        }
+
     private void percevoirRessource(){
         //TODO implémenter la méthode
     }
