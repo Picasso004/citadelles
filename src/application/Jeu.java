9f236f4060a6539eeca7df9ae6f16626834136a6
aminata <adiop.ir2025@esaip.org>
2023-12-08 11:56:30 +0100

    regle de jeu

--- src/application/Jeu.java
+++ src/application/Jeu.java
@@ -51,10 +51,161 @@ public class Jeu {
         }while (choix != 3);
 
     }
-    private void afficherLesRegles(){
-        //TODO implémenter la méthode
-        System.out.println("Voici les règles du jeu citadelles.");
+    private void afficherLesRegles() {
+        System.out.println("Bienvenue dans Citadelle !");
+        System.out.println("Objectif du jeu : Construisez une cité de Sept quartiers avant vos adversaires.");
+
+        // Ajout des règles de distribution des cartes Personnage en début de partie
+        System.out.println("\nDistribution des cartes Personnage en début de partie :");
+        System.out.println("- Le joueur qui détient la Couronne mélange les 8 cartes Personnage choisies.");
+        System.out.println("- Il écarte une carte face cachée.");
+        System.out.println("- Il écarte 2 cartes face visible en fonction du nombre de joueurs.");
+        System.out.println("- Les cartes écartées ne seront pas utilisées pour le tour.");
+        System.out.println("- Le joueur possédant la Couronne choisit secrètement une des cartes parmi les cartes Personnage restantes.");
+        System.out.println("- Il place cette carte devant lui face cachée.");
+        System.out.println("- Il passe ensuite les cartes Personnage restantes à son voisin de gauche qui procède de la même manière.");
+        System.out.println("- Le dernier joueur place la dernière carte restante face cachée, avec la première carte écartée face cachée.");
+
+        // Description des 14 merveilles
+        System.out.println("\nDéscription des 14 merveilles :");
+        System.out.println("1. Bibliothèque effet : revenu (cartes quartier), coût de construction : 6\n" +
+                "Si vous choisissez de piocher des cartes au début du tour, concervez-les toutes");
+
+        System.out.println("2.Carrière effet : construction, coût de construction : 5\n" +
+                "Vous pouvez bâtir des quartiers identiques à d’autres quartiers de votre cité. Le propriétaire\n" +
+                "de la carrière peut bâtir autant de quartiers identiques qu’il le souhaite, mais ne peut pas\n" +
+                "utiliser le pouvoir de l’´Echevin, du Diplomate ou du Capitaine pour acquérir des quartiers\n" +
+                "identiques.");
+
+        System.out.println("3.Cours des Miracles effet : calcul des points, coût de construction : 2\n" +
+                "Pour le calcul du score final, la Cour des Miracles est considérée comme un quartier de type\n" +
+                "(couleur) de votre choix. Dans la cas ou le propriétaire la considère comme un quartier noble,\n" +
+                "militaire, marchant ou religieux, la Cour des Miracles ne peut plus ˆetre consid´er´ee comme\n" +
+                "une merveille.");
+
+        System.out.println("4. Donjon effet : pouvoir, coût de construction : 3\n" +
+                            "Le Donjon ne peut être affecté par les pouvoirs des personnages de rang 8");
+
+        System.out.println("5. Dracoport effet : calcul des points, coût de construction : 6\n" +
+                            "Marquez 2 points supplémentaires à la fin de la partie");
+
+        System.out.println("6.Ecole de Magie effet : revenu (or), coût de construction : 6\n" +
+                            "Pour la perception des revenus des personnages, l’école de Magie est considérée comme un\n" +
+                            "quartier du type (couleur) de votre choix.");
+
+        System.out.println("7. Fontaine aux Souhaits effet : calcul des points, coût de construction : 5\n" +
+                            "`A la fin de la partie, marquez 1 point supplméntaire par merveille dans votre cité, y compris\n" +
+                             "la Fontaine aux Souhaits.");
+
+        System.out.println("8.Forge effet : revenu (cartes quartier), coût de construction : 5\n" +
+                            "Une fois par tour, vous pouvez payez 2 pièces d’or pour piocher 3 cartes.");
+
+        System.out.println("9. Laboratoire effet : fin tour, coût de construction : 5\n" +
+                            "Une fois par tour, vous pouvez d´efausser 1 carte pour recevoir 2 pièces d’or.");
+
+        System.out.println("10.Manufacture effet : construction, coût de construction : 5\n" +
+                            "Payez 1 pièce d’or de moins lorsque vous bâtissez une autre merveille.");
+
+        System.out.println("11. Salles des Cartes effet : calcul des points, coût de construction : 5\n" +
+                            "`A la fin de la partie, marquez 1 point supplémentaire par carte dans votre main.");
+
+        System.out.println("12. Statue Equestre effet : calcul des points, coût de construction : 3\n" +
+                            "Si vous d´etenez le Couronne à la fin de la partie, marquez 5 points supplémentaires.");
+
+        System.out.println("13. Trésor Impérial effet : calcul des points, coût de construction : 5\n" +
+                            "`A la fin de la partie, marquez 1 point supplémentaire par pièce d’or dans votre trésor.");
+
+        System.out.println("14. Tripot effet : construction, coût de construction : 6\n" +
+                            "Vous pouvez payer tout ou partie du coût de construction du Tripot en cartes de votre main,\n" +
+                            "au prix de 1 carte pour 1 pièce d’or. Si le Tripot est confisqué par l’Echevin, le joueur n’est\n" +
+                            "remboursé que de l’or qu’il a dépensé, pas des cartes.");
+
+        //
+
+        System.out.println("\nDéscription des personnages :");
+
+        System.out.println("1- Assassin : Annoncez le personnage que vous assassinez. Si un joueur a ce personnage, il ne doit\n" +
+                "pas réagir tout de suite, mais lorsque le personnage assassiné sera appelé. Le personnage\n" +
+                "assassiné passe son tour.");
+
+        System.out.println("2- Voleur:  Annoncez le personnage que vous volez. Si un joueur a ce personnage, il ne doit pas\n" +
+                "réagir. Lorsque le personnage volé sera appelé et révélé, vous lui prendrez toutes ces pièces\n" +
+                "d’or. Vous ne pouvez voler ni le personnage de rang 1 (Assassin, Sorcière ou Echevin), ni le\n" +
+                "personnage assassiné ou ensorcelé.");
+
+        System.out.println("3- Magicienne : Vous pouvez au choix :\n" +
+                "— soit échanger toutes les cartes de votre main avec celles d’un autre joueur ; si vous n’avez\n" +
+                "aucune carte en main, vous prenez simplement les cartes de l’autre joueur,\n" +
+                "— soit d´efausser un certain nombre de cartes de votre main, les placer sous la pioche et\n" +
+                "piocher le même nombre de cartes du dessus de la pioche.");
+
+        System.out.println("4- Roi: Recevez une pièce d’or pour chaque quartier Noble dans votre cité.\n" +
+                "Vous devez prendre la Couronne. C’est désormais vous qui appelerez les personnages. En\n" +
+                "outre, au prochain tour, et aux tours suivants jusqu’à ce qu’un autre joueur révèle le Roi,\n" +
+                "vous choisirez votre personnage en premier.\n" +
+                "Si vous ˆetes assassiné, vous passez votre tour comme n’importe quel personnage. Néanmoins,\n" +
+                "après que tous les autres joueurs ont joué, vous révélez que vous aviez choisi le Roi et, en\n" +
+                "tant qu’héritier du Roi assassiné, prenez la Couronne.\n" +
+                "Si vous êtes ensorcelé, vous prenez quand même la Couronne.\n" +
+                "Si le Roi est écarté face visible en début de tour, remplacez-le par le personnage suivant et\n" +
+                "mélangez-le avec les personnages restants.");
+
+
+        System.out.println("5- Evêque: Recevez 1 pièce d’or pour chaque quartier religieux dans votre cité.\n" +
+                "Vos quartiers ne peuvent pas être affectés par les pouvoirs des personnages de rang 8 \n" +
+                "(Condottiere, Diplomate, Capitaine).\n" +
+                "Si vous êtes assassiné, vous n’êtes plus ´Evêque ; vous êtes mort, et vos Quartiers peuvent\n" +
+                "donc être la cible des personnages de rang 8. De même, si vous êtes ensorcelé, les quartiers\n" +
+                "de la Sorcière sont protégés contre les personnages de rang 8, mais pas les vôtres..");
+
+        System.out.println("6- Marchande : Recevez 1 pièce d’or pour chaque quartier Commerçant dans votre cité.\n" +
+                "Recevez 1 pièce d’or supplémentaire quel que soit le type de ressources (pièce d’or ou carte)\n" +
+                "que vous avez prises au début de votre tour.");
+
+        System.out.println("7- Architecte :Piochez 2 cartes Quartier et ajoutez-les à votre main.\n" +
+                "Vous pouvez bâtir jusqu’à 3 quartiers durant votre tour.");
+
+        System.out.println("8- Condottiere: Recevez 1 pièce d’or pour chaque quartier Militaire dans votre cité.\n" +
+                "Vous pouvez détruire un quartier de votre choix dans une cité en payant son coût de construction\n" +
+                " moins 1. Vous pouvez donc détruire gratuitement un quartier de coût 1, ou payer 1 pièce\n" +
+                "d’or pour détruire un quartier de coût 2, etc.\n" +
+                "Vous ne pouvez détruire un quartier d’une cité déjà complète. Vous pouvez détruire l’un de\n" +
+                "vos propres quartiers. Les quartiers détruits sont défaussés, face cachée, en dessous de la\n" +
+                "pioche.");
+
+
+
+
+
+
+        System.out.println("\nDéroulement du tour de jeu :");
+        System.out.println("1. Choisissez un personnage parmi ceux disponibles.");
+        System.out.println("2. Réalisez les actions spécifiques à ce personnage.");
+        System.out.println("3. Obtenez des pièces d'or ou piochez des cartes en fonction de votre choix.");
+        System.out.println("4. Construisez des quartiers pour compléter votre cité.");
+
+
+        System.out.println("\nGestion de la couronne :");
+        System.out.println("- Le joueur qui choisit le personnage Roi reçoit la couronne.");
+        System.out.println("- La couronne confère des avantages pendant le tour.");
+
+        System.out.println("\nFin de la partie :");
+        System.out.println("- La partie se termine lorsque quelqu'un a une cité de Sept quartiers.");
+        System.out.println("- Le joueur avec la cité complète gagne la partie 4 points supplementaire.");
+        // Ajout des règles de calcul des points à la fin de la partie
+        System.out.println("\nCalcul des points à la fin de la partie :");
+        System.out.println("- Somme totale des coûts de construction des quartiers de votre cité.");
+        System.out.println("- 3 points supplémentaires si votre cité comprend au moins un quartier de cinq types différents (Noble, Commerçant, Religieux, Militaire et Merveille).");
+        System.out.println("- 4 points supplémentaires si vous êtes le premier joueur ayant complété votre cité.");
+        System.out.println("- 2 points supplémentaires si votre cité est complète mais que vous n'avez pas été le premier à la compléter.");
+        System.out.println("- Somme des différents bonus éventuels des Merveilles de votre cité.");
+
+        System.out.println("\nLe joueur avec le score le plus élevé est le vainqueur.");
+        System.out.println("En cas d'égalité, la victoire revient à celui qui a révélé le personnage de rang le plus élevé au dernier tour.");
+
+        // Ajoutez ici d'autres instructions pour expliquer des règles spécifiques, des cartes spéciales, etc.
     }
+
     private void jouerPartie(){
         initialisation();
         int i = 1;
