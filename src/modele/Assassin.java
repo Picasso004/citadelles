b2852bcf0b28c3a3336122c588361b89f8e8d815
Picasso004 <sanvee34@gmail.com>
2023-12-12 23:29:31 +0100

    MAJ Assassin

--- src/modele/Assassin.java
+++ src/modele/Assassin.java
@@ -1,5 +1,7 @@
 package modele;
 
+import controleur.Interaction;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,36 +14,29 @@ public class Assassin extends Personnage {
     @Override
     public void utiliserPouvoir() {
         System.out.println("Quel personnage voulez-vous assassiner ?");
-
-        Scanner scanner = new Scanner(System.in);
         PlateauDeJeu plateau = this.getPlateau();
 
         for (int i = 0; i < plateau.getNombrePersonnages(); i++) {
             Personnage personnage = plateau.getPersonnage(i);
             if(personnage.getJoueur() != null){
-                System.out.println((i + 1) + " " + personnage.getNom());
+                System.out.println((i + 1) + "- " + personnage.getNom());
             }
         }
 
         int choix;
         do {
             System.out.print("Votre choix : ");
-            choix = scanner.nextInt();
+            choix = Interaction.lireUnEntier(1,plateau.getNombrePersonnages()+1);
             choix--; // Convertir le choix de l'utilisateur en indice
-            if (choix >= 0 && choix < plateau.getNombrePersonnages()) {
-                if (plateau.getPersonnage(choix) == this) {
-                    System.out.println("Vous ne pouvez pas vous assassiner.");
-                } else {
-                    plateau.getPersonnage(choix).setAssassine();
-                    System.out.println("Le personnage " + plateau.getPersonnage(choix).getNom() + " a été assassiné.");
-                    break;
-                }
+
+            if (plateau.getPersonnage(choix) == this) {
+                System.out.println("\nVous ne pouvez pas vous assassiner.");
             } else {
-                System.out.println("Choix invalide. Réessayez.");
+                plateau.getPersonnage(choix).setAssassine();
+                System.out.println("Le personnage " + plateau.getPersonnage(choix).getNom() + " a été assassiné.");
+                break;
             }
         } while (true);
-
-        scanner.close();
     }
 
     @Override
